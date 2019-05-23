6e90de012eb1d70992bd3c24a794c9e8df36c65e
laytommy <lay.thompsonn@gmail.com>
2019-05-23 17:42:12 -0400

    changes to index.html > added in IDs, section discover in progress

--- script/script.js
+++ script/script.js
@@ -8,6 +8,7 @@
 movieShowApp.getMovies = function (){
   console.log(`pizza is ready`);
 
+  // Call for MOVIES > Top Rated.
   $.ajax({
     url: `https://api.themoviedb.org/3/movie/top_rated`,
     method: `GET`,
@@ -26,23 +27,54 @@ movieShowApp.getMovies = function (){
 //     movieShowApp.displayMovie(movies)
   })
 
-  $.ajax({
-    url: `https://api.themoviedb.org/3/tv/top_rated`,
-    method: `GET`,
-    datatype: `json`,
-    data: {
-      api_key: movieShowApp.key
-    }
-  }).then(function (tvResults){
-    console.log(tvResults);
-  })
+  // Call for TV SHOWS > Top Rated.
+  // $.ajax({
+  //   url: `https://api.themoviedb.org/3/tv/top_rated`,
+  //   method: `GET`,
+  //   datatype: `json`,
+  //   data: {
+  //     api_key: movieShowApp.key
+  //   }
+  // }).then(function (tvResults){
+  //   console.log(tvResults);
+  // })
 }
 
-movieShowApp.getMovies()
+// Start button on submit > scroll down to discover section.
+$(`.start`).on(`submit`, function (event) {
+  // Prevents button default
+  event.preventDefault();
+  // Scroll to element .discover
+  $(`html, body`).animate({
+    scrollTop: $(`#discover`).offset().top
+  }, 1000);
+})
 
-// movieShowApp.displayMovie = function(arrayOfMovies){
-//   console.log(arrayOfMovies);
+// Provide users with choices: Movie or TV Shows
+$(`.discoverContainer`).html (
+  <div class="movies">
+    <h2>Movies</h2>
+  </div>
+  <div class="tvShows">
+    <h2>TV Shows</h2>
+  </div>
+)
 
-// }
+movieShowApp.displayMovie = function(arrayOfMovies){
+  console.log(arrayOfMovies);
+
+}
+
+// Init method holds anything that needs to be run at load times + even listeners.
+movieShowApp.init = function (){
+
+  movieShowApp.getMovies()
+}
+
+// Document READY. Wait till everything is loaded successfully.
+$(function () {
+  // Start/ begin movieShowApp
+  movieShowApp.init();
+})
 
 
