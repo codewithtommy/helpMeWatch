f229c639941e094ad576a59a5a79bba22808cd64
laytommy <lay.thompsonn@gmail.com>
2019-05-25 15:45:36 -0400

    styling 80-90% complete? @media query in progress -tommy/ jamie @ class

--- script/script.js
+++ script/script.js
@@ -4,50 +4,46 @@ movieShowApp = {};
 
 // API key
 movieShowApp.key = `cca8aaccc614149518ad4b0a721a135a`; 
-// randomize movies*********
-// randomize the array, need to randomize the array here.
+
+// global variable to for randomization for movie (drama/ comedy)
 movieShowApp.randomMovie = (movieDramaAll) => {
   randomM = movieDramaAll[Math.floor(Math.random() * movieDramaAll.length)];
 }
-// for comedy
+
 movieShowApp.randomMovie = (movieComedyAll) => {
   randomM = movieComedyAll[Math.floor(Math.random() * movieComedyAll.length)];
 }
-// randomize tv shows ************
-// randomize drama
+
+// global variable to for randomization for tv shows (drama/ comedy)
 movieShowApp.randomTv = (tvDramaAll) =>{
   randomT = tvDramaAll[Math.floor(Math.random() * tvDramaAll.length)];
-  // console.log(randomT);
 }
-// randomize comedy
+
 movieShowApp.randomTv = (tvComedyAll) => {
   randomT = tvComedyAll[Math.floor(Math.random() * tvComedyAll.length)];
-  // console.log(randomT);
 }
 
 // AJAX call for Movies and TV shows.
 movieShowApp.getMovies = function (){
-  // call for Movies API.
+  // call for Movies API with drama.
   $.ajax({
-    // might have to change the url to just '18' for drama and then make a seperate call for '35', but let's see what we get first
     url: `https://api.themoviedb.org/3/discover/movie?language=en-US&with_genres=18`,
     method: `GET`,
     datatype:`json`,
     data: {
       api_key: movieShowApp.key
     }
+
     }).then(function (movieResults){
-    // console.log(movieResults);
-    // we make movieResults.results into a variable 'test'. we can change the name later
       const movieDramaAll = movieResults.results;
-      // console.log(movieDramaAll);
       // we'll get this an array now, since we get the data from the ajax call as an object
       // console.log(test)
       // we then need to randomize our movie before the display, so we have a function created above to randomize the movie into this, then call it below
       movieShowApp.randomMovie(movieDramaAll)
   });
+
+  // call for Movies API with comedy
   $.ajax({
-    // might have to change the url to just '18' for drama and then make a seperate call for '35', but let's see what we get first
     url: `https://api.themoviedb.org/3/discover/movie?language=en-US&with_genres=35`,
     method: `GET`,
     datatype: `json`,
@@ -58,10 +54,9 @@ movieShowApp.getMovies = function (){
       const movieComedyAll = movieResults.results;
       movieShowApp.randomMovie(movieComedyAll)
   });
-// end of movie calls ***************************************************************
-// start of tv calls *****************************************************************
+
+  // call for TV Shows API with drama.
   $.ajax({
-    // might have to change the url to just '18' for drama and then make a seperate call for '35', but let's see what we get first
     url: ` https://api.themoviedb.org/3/discover/tv?language=en-US&sort_by=popularity.desc&page=1&with_genres=18`,
     method: `GET`,
     datatype: `json`,
@@ -69,15 +64,11 @@ movieShowApp.getMovies = function (){
       api_key: movieShowApp.key
     }
   }).then(function (tvResults) {
-    // console.log(movieResults);
-    // we make movieResults.results into a variable 'test'. we can change the name later
     const tvDramaAll = tvResults.results;
-    console.log(tvDramaAll);
-    // we'll get this an array now, since we get the data from the ajax call as an object
-    // console.log(test)
-    // we then need to randomize our movie before the display, so we have a function created above to randomize the movie into this, then call it below
     movieShowApp.randomTv(tvDramaAll)
     });
+
+  // call for TV Shows API with comedy.
   $.ajax({
     // might have to change the url to just '18' for drama and then make a seperate call for '35', but let's see what we get first
     url: ` https://api.themoviedb.org/3/discover/tv?language=en-US&sort_by=popularity.desc&page=1&with_genres=35`,
@@ -87,26 +78,19 @@ movieShowApp.getMovies = function (){
       api_key: movieShowApp.key
     }
   }).then(function (tvResults) {
-    // console.log(movieResults);
-    // we make movieResults.results into a variable 'test'. we can change the name later
     const tvComedyAll = tvResults.results;
-    console.log(tvComedyAll);
-    // we'll get this an array now, since we get the data from the ajax call as an object
-    // console.log(test)
-    // we then need to randomize our movie before the display, so we have a function created above to randomize the movie into this, then call it below
     movieShowApp.randomTv(tvComedyAll)
   });
-// deleted tvShows call for ajax for now to make things less messy... can copy paste then re-adjust from our first call.
 }
 
-
 // HEADER: start button on submit > scroll down to discover section.
+
 // resets quiz
-$(`#resetQuiz`).on(`click`,function(event){
+$(`#reset`).on(`click`,function(event){
   location.reload();
 })
 // hide reset button
-$('#resetQuiz').css(`display`,'none')
+$('#reset').css(`display`,'none')
 $(`.start`).on(`click`, function (event) {
   // Prevents button default
   event.preventDefault();
@@ -114,7 +98,7 @@ $(`.start`).on(`click`, function (event) {
   // Disable button to prevent multiple submits
   $(`.start`).attr(`disabled`, true);
   $(`.start`).css(`display`, 'none');
-  $(`#resetQuiz`).css(`display`,'initial');
+  $(`#reset`).css(`display`,'initial');
 
   // Scroll to element .discover
   $(`html, body`).animate({
@@ -200,24 +184,24 @@ $(`.formTwo`).on(`submit`, function (event) {
   const tvDramaChecked = ($('#tvShows:checked').val() && $('#drama:checked').val());
   const tvComedyChecked = ($('#tvShows:checked').val() && $('#comedy:checked').val());
 // if nothing is checked
-   
+
   // IF Movies && Drama are checked OR Movies && Comedy are checked... append...
   if (movieDramaChecked) {
       const moviePoster = $(`<img>`).attr('src', 'https://image.tmdb.org/t/p/w500'+`${randomM.poster_path}`).attr('alt',randomM.title)
     $(`.resultText`).html(
       `
-        <h2>${randomM.title}</h2>
-        <h3>${randomM.overview}</h3>
+        <h3>${randomM.title}</h3>
+        <h4>${randomM.overview}</h4>
       `
     )
       $(`.resultImg`).append(moviePoster);
 
   } else if(movieComedyChecked){
-      const moviePoster = $('.resultImg').prepend($(`<img>`)).attr('src', 'https://image.tmdb.org/t/p/w500' + `${randomM.poster_path}`).attr('alt',randomM.title)
+      const moviePoster = $(`<img>`).attr('src', 'https://image.tmdb.org/t/p/w500' + `${randomM.poster_path}`).attr('alt',randomM.title)
     $(`.resultText`).html(
       `
-        <h2>${randomM.title}</h2>
-        <h3>${randomM.overview}</h3>
+        <h3>${randomM.title}</h3>
+        <h4>${randomM.overview}</h4>
       `
     ) 
       $(`.resultImg`).append(moviePoster);
@@ -231,8 +215,8 @@ $(`.formTwo`).on(`submit`, function (event) {
     const tvPoster = $(`<img>`).attr('src', 'https://image.tmdb.org/t/p/w500' + `${randomT.poster_path}`).attr('alt',randomT.name)
     $(`.resultText`).html(
       `
-        <h2>${randomT.name}</h2>
-        <h3>${randomT.overview}</h3>
+        <h3>${randomT.name}</h3>
+        <h4>${randomT.overview}</h4>
       `
     )
       $(`.resultImg`).append(tvPoster);
@@ -243,8 +227,8 @@ $(`.formTwo`).on(`submit`, function (event) {
     const tvPoster = $(`<img>`).attr('src', 'https://image.tmdb.org/t/p/w500' + `${randomT.poster_path}`).attr('alt', randomT.name)
     $(`.resultText`).html(
       `
-        <h2>${randomT.name}</h2>
-        <h3>${randomT.overview}</h3>
+        <h3>${randomT.name}</h3>
+        <h4>${randomT.overview}</h4>
       `
     )
     $(`.resultImg`).append(tvPoster);
@@ -255,7 +239,6 @@ $(`.formTwo`).on(`submit`, function (event) {
     
   })
 })
-
 
 // init method holds anything that needs to be run at load times + even listeners.
 movieShowApp.init = function (){
